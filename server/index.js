511fd7e4f960a5e4c1d9108f17513cd14f5cc21b
Minh Pham <minh.pham@quant-edge.com>
2018-11-14 13:24:28 +0700

    config proxy'

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 3001;
 const router = express.Router();
-router.get("/", (req, res) => {
+router.get("/note", (req, res) => {
     res.send({ response: "I am alive" }).status(200);
 });
 const app = express();
@@ -18,5 +18,14 @@ io.on("connection", socket => {
         io.emit("message", msg);
     });
 });
+
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+//   }
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
